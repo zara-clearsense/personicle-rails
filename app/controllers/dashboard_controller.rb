961eabe9ead8acde9f88b15dc1f8e31574ad4053
tirth-clearsense <tpatel@clearsense.com>
2022-03-16 11:48:51 -0500

    timeline bug fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,11 +5,11 @@ class DashboardController < ApplicationController
   before_action :require_user
   
   def index
+    
     three_months_ago = 3.months.ago.strftime("%Y-%m-%d %H:%M:%S.%6N")
     current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
-
     url = ENV['EVENTS_ENDPOINT']+"?user_id="+"#{session[:oktastate]["uid"]}"+"&startTime="+three_months_ago+"&endTime="+current_time
-    
+   
     res = RestClient::Request.execute(:url => url, headers: {Authorization: "Bearer #{session[:oktastate]['credentials']['token']} "}, :method => :get, :verify_ssl => false)
     if res
       @response = JSON.parse(res,object_class: OpenStruct)
--- app/javascript/components/useGoogleCharts.js
+++ app/javascript/components/useGoogleCharts.js
@@ -6,10 +6,11 @@ function useGoogleCharts () {
   const [google, setGoogle] = useState(null);
   
   useEffect(() => {
-    // if (!google) {
+ 
+    if (!google) {
       const head = document.head;
       let script = document.getElementById('googleChartsScript');
-      if (!script) {
+      
         script = document.createElement('script');
         script.src = 'https://www.gstatic.com/charts/loader.js';
         script.id = 'googleChartsScript';
@@ -21,19 +22,17 @@ function useGoogleCharts () {
           }
         };
         head.appendChild(script);
-      } else if (window.google && window.google.charts && window.google.visualization) {
-        setGoogle(window.google);
-      }
-    // }
+       
+    }
 
     return () => {
+
       let script = document.getElementById('googleChartsScript');
       // if (script) {
       //   script.remove();
       // }
     }
   }, [google]);
-
   return google;
 }
 
