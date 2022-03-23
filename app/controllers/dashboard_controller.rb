b04092556370636130b8be6fb5943e4cbfcd95cc
tirth-clearsense <tpatel@clearsense.com>
2022-03-23 12:22:48 -0500

    health labels added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,12 +6,13 @@ class DashboardController < ApplicationController
   
   def index
     # puts session[:oktastate]['credentials']['token']
-    # puts session[:oktastate]["uid"]
+    # puts session[:oktastate][0]['token']
     three_months_ago = 3.months.ago.strftime("%Y-%m-%d %H:%M:%S.%6N")
     current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
-    url = ENV['EVENTS_ENDPOINT']+"?user_id="+"#{session[:oktastate]["uid"]}"+"&startTime="+three_months_ago+"&endTime="+current_time
-   
+    url = ENV['EVENTS_ENDPOINT']+"?user_id="+"#{session[:oktastate]['uid']}"+"&startTime="+three_months_ago+"&endTime="+current_time
+    # url = "http://127.0.0.1:8000/request/events"+"?user_id="+"#{session[:oktastate][1]}"+"&startTime="+three_months_ago+"&endTime="+current_time
     res = RestClient::Request.execute(:url => url, headers: {Authorization: "Bearer #{session[:oktastate]['credentials']['token']} "}, :method => :get, :verify_ssl => false)
+   
     if res
       @response = JSON.parse(res,object_class: OpenStruct)
     end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     
     def oktaoauth
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
+      # user.rb
+      @user = User.from_omniauth(request.env["omniauth.auth"].except("extra") )
       
       if @user.save
         session[:oktastate] = request.env["omniauth.auth"].except("extra")
-        # puts request.env["omniauth.auth"].except("extra")
+        
       else
         print(@user.errors.full_messages)
       end
