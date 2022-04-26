26d500aa2d8ba59921de8ad54709373d2e924252
Vaibhav Pandey <vpandey@clearsense.com>
2022-04-26 12:14:15 -0700

    added dynamic sleep trends

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,9 @@ class DashboardController < ApplicationController
   require 'ostruct'
   require 'date'
   before_action :require_user, :session_active?
-  
+  # Load all events data
+  # Find average sleep duration for last week + last month
+
   def index
     
     three_months_ago = 3.months.ago.strftime("%Y-%m-%d %H:%M:%S.%6N")
@@ -15,6 +17,10 @@ class DashboardController < ApplicationController
     if res
       # puts "hello"
       @response = JSON.parse(res,object_class: OpenStruct)
+      # last_month_total_sleep, last_month_sleep_events
+      # last_week_total_sleep, last_week_sleep_events
+      @last_week_average_sleep = 9.5
+      @last_month_average_sleep = 8
     end
     
   end
