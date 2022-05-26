f183577445616a6ade5a1af2fb1d43a91d7f55f6
tirth-clearsense <tpatel@clearsense.com>
2022-05-26 10:41:28 -0400

    delete account

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,17 @@ class ProfileController < ApplicationController
   before_action :require_user, :session_active?
     
     def index
+      if not params[:delete_account].blank? and params[:delete_account] == "delete"
+        url = ENV['ACCOUNT_DELETE']
+        res = RestClient::Request.execute(:url => url, headers: {Authorization: "Bearer #{session[:oktastate]['credentials']['token']} "}, :method => :delete,:verify_ssl => false )
+        print(res.code)
+        print(res)
+        if res.code == 200
+          session[:oktastate] = nil
+          flash[:warning] = "Your account is successfully deleted"
+          redirect_to root_path
+        end
+      end
     end
   end
   
