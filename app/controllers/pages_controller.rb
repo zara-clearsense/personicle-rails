a18f6b840e22a44c55f0b9f258b1aab83dbe1b5d
tirth-clearsense <tpatel@clearsense.com>
2022-06-22 22:12:14 -0400

    redirect to app.personicle.org

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,7 @@ class PagesController < ApplicationController
     # end
     case action_name
     when 'login', 'register'
+      redirect_to "https://app.personicle.org/"
       'authentication'
     else
       'dashboard'
