c833a63a042a33c0b55622a5a32e5b0a7155ad4b
zarskiahmed <12za3@queensu.ca>
2022-08-18 17:39:37 -0400

    Exposome + Dropdown - fix physician_responses

--- app/controllers/exposome_controller.rb
+++ app/controllers/exposome_controller.rb
@@ -5,39 +5,39 @@ class ExposomeController < ApplicationController
     before_action :require_user, :session_active?
 
     def index
-       @exposome_streams = [
-        "com.personicle.individual.datastreams.exposome.aqi",
-        "com.personicle.individual.datastreams.exposome.no",
-        "com.personicle.individual.datastreams.exposome.no2",
-        "com.personicle.individual.datastreams.exposome.03",
-        "com.personicle.individual.datastreams.exposome.so2",
-        "com.personicle.individual.datastreams.exposome.pm2_5",
-        "com.personicle.individual.datastreams.exposome.pm10",
-        "com.personicle.individual.datastreams.exposome.nh3",
-        "com.personicle.individual.datastreams.exposome.temp",
-        "com.personicle.individual.datastreams.exposome.feels_like",
-        "com.personicle.individual.datastreams.exposome.pressure",
-        "com.personicle.individual.datastreams.exposome.humidity",
-        "com.personicle.individual.datastreams.exposome.uvi"
-       ]
+        @exposome_streams = [
+            ["aqi", "com.personicle.individual.datastreams.exposome.aqi"],
+            ["no", "com.personicle.individual.datastreams.exposome.no"],
+            ["no2", "com.personicle.individual.datastreams.exposome.no2"],
+            ["03", "com.personicle.individual.datastreams.exposome.03"],
+            ["so2", "com.personicle.individual.datastreams.exposome.so2"],
+            ["pm2_5", "com.personicle.individual.datastreams.exposome.pm2_5"],
+            ["pm10", "com.personicle.individual.datastreams.exposome.pm10"],
+            ["nh3", "com.personicle.individual.datastreams.exposome.nh3"],
+            ["temp", "com.personicle.individual.datastreams.exposome.temp"],
+            ["feels_like", "com.personicle.individual.datastreams.exposome.feels_like"],
+            ["pressure", "com.personicle.individual.datastreams.exposome.pressure"],
+            ["humidity", "com.personicle.individual.datastreams.exposome.humidity"],
+            ["uvi", "com.personicle.individual.datastreams.exposome.uvi"]
+           ]
     end
 
     def get_exposome_data
         required = [ :exposomeType]
         @exposome_streams = [
-        "com.personicle.individual.datastreams.exposome.aqi",
-        "com.personicle.individual.datastreams.exposome.no",
-        "com.personicle.individual.datastreams.exposome.no2",
-        "com.personicle.individual.datastreams.exposome.03",
-        "com.personicle.individual.datastreams.exposome.so2",
-        "com.personicle.individual.datastreams.exposome.pm2_5",
-        "com.personicle.individual.datastreams.exposome.pm10",
-        "com.personicle.individual.datastreams.exposome.nh3",
-        "com.personicle.individual.datastreams.exposome.temp",
-        "com.personicle.individual.datastreams.exposome.feels_like",
-        "com.personicle.individual.datastreams.exposome.pressure",
-        "com.personicle.individual.datastreams.exposome.humidity",
-        "com.personicle.individual.datastreams.exposome.uvi"
+        ["aqi", "com.personicle.individual.datastreams.exposome.aqi"],
+        ["no", "com.personicle.individual.datastreams.exposome.no"],
+        ["no2", "com.personicle.individual.datastreams.exposome.no2"],
+        ["03", "com.personicle.individual.datastreams.exposome.03"],
+        ["so2", "com.personicle.individual.datastreams.exposome.so2"],
+        ["pm2_5", "com.personicle.individual.datastreams.exposome.pm2_5"],
+        ["pm10", "com.personicle.individual.datastreams.exposome.pm10"],
+        ["nh3", "com.personicle.individual.datastreams.exposome.nh3"],
+        ["temp", "com.personicle.individual.datastreams.exposome.temp"],
+        ["feels_like", "com.personicle.individual.datastreams.exposome.feels_like"],
+        ["pressure", "com.personicle.individual.datastreams.exposome.pressure"],
+        ["humidity", "com.personicle.individual.datastreams.exposome.humidity"],
+        ["uvi", "com.personicle.individual.datastreams.exposome.uvi"]
        ]
 
         form_complete = true
@@ -53,6 +53,7 @@ class ExposomeController < ApplicationController
         et = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
 
         exposome_type = params[:exposomeType]
+        puts exposome_type
 
         # For each call of this API, need to store this data in hash variable / hash object
         # @exposome_streams_hash = {}
@@ -63,11 +64,15 @@ class ExposomeController < ApplicationController
                 exposome_data = @response
                 # -- all the views will be able to access that hash object, so when generating chart for temperature
                 # can pass object specific for temperature
-                # In chart, use time stamp and value fields to generate the chart. 
+                # In chart, use time stamp and value fields to generate the chart.
+                puts "real exposome"
+                puts @exposome_streams[0] 
             else   
                 @response = FetchData.get_datastreams(session,source="org.personicle.exposome",data_type=exposome_type,start_date=st, end_date=et, hard_refresh=false,uid=session[:oktastate]['uid'])
                 # @exposome_streams_hash[exposome_type] = @response
                 exposome_data = @response
+                puts "real exposome"
+                puts @exposome_streams[0]
             end
 
             if !@response.empty?
@@ -75,7 +80,8 @@ class ExposomeController < ApplicationController
             else
             end
 
-            puts exposome_data
+            
+            #puts exposome_data
             respond_to do |format|
                 format.html {render :index, locals: { exposome_data: exposome_data, exposome_type: exposome_type } }
                 # redirect_to pages_recipes_path, recipes: response.hits
