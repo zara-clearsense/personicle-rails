9f8b67856cbf1351b7a1466b6999ec65e62d1c3e
zarskiahmed <12za3@queensu.ca>
2022-10-25 16:56:15 -0400

    Update and Delete Selected buttons working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -128,11 +128,11 @@ class DashboardController < ApplicationController
     # url = "https://api.personicle.org/data/write/event/delete"?user_id=userid&event_id=some_event_id;another_event_id
     # RestClient::Request.execute(:url => url, headers: {Authorization: "Bearer #{session[:oktastate]['credentials']['token']} "}, :method => :delete, ),object_class: OpenStruct)
     # params[:select-all] = select-all_value
-    events = params[:selected_events]
+    events = JSON.parse(params[:selected_events])
  
     if !events.nil?
       events = events.join(";")
-      url = "https://api.personicle.org/data/write/event/delete?user_id=#{session[:oktastate]['uid']}&event_id=#{events}"
+      url = "https://staging.personicle.org/data/write/event/delete?user_id=#{session[:oktastate]['uid']}&event_id=#{events}"
       res =  JSON.parse(RestClient::Request.execute(:url => url, headers: {Authorization: "Bearer #{session[:oktastate]['credentials']['token']} "}, :method => :delete,:verify_ssl => false ),object_class: OpenStruct)
       redirect_to pages_dashboard_path, refresh:"hard_refresh"
     end
@@ -142,7 +142,8 @@ class DashboardController < ApplicationController
   # Inside update make 2 API calls
   # 1. Delete event api
   # 2. Add events api
-    events = params[:selected_events]
+    events = JSON.parse(params[:selected_events])
+    puts events
   
     if !events.nil?
       events = events.join(";")
