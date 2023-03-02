e9348ecb7226bc0afe4b9dd69010284b9d8a8617
tirth-clearsense <tpatel@clearsense.com>
2023-03-02 04:28:49 -0800

    show average value in weekly view for numeric responses

--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -54,8 +54,8 @@ class UserResponsesController < ApplicationController
 
     def index
         @user_questions = User.find_by(user_id: session[:oktastate]['uid']).questions
-        puts "User Questions"
-        puts @user_questions
+        # puts "User Questions"
+        # puts @user_questions
         @user_questions["questions"].each {|element| puts element["question"]}
 
         @questions = {}
@@ -64,10 +64,10 @@ class UserResponsesController < ApplicationController
         @user.physicians.each do |phy|
             
             @physician_questions = PhysicianUser.find_by(user_user_id: session[:oktastate]['uid'], physician_user_id: phy.user_id).questions
-            puts @physician_questions.to_yaml
+            # puts @physician_questions.to_yaml
             if !@physician_questions.empty? 
             @questions[[phy.id,phy.name]] = @physician_questions if !@physician_questions.empty?
-            puts @questions[[phy.id,phy.name]]
+            # puts @questions[[phy.id,phy.name]]
             @physician_questions["questions"].each {|ques| puts ques["question"]}
             end
         end
