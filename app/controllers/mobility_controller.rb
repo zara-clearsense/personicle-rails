c34e49b661451a0888df3e5e89c9c2944ce83557
zarskiahmed <12za3@queensu.ca>
2023-03-21 02:38:49 -0400

    Daily chart 12-month carousel (dates with 0 steps added on graph) + stylesheet commented out (needed for left and right controls to work)

--- app/controllers/mobility_controller.rb
+++ app/controllers/mobility_controller.rb
@@ -10,7 +10,7 @@ class MobilityController < ApplicationController
     # end
     # add another request for specific data streams (step count)
     # use exercise for another chart
-    st = 3.months.ago.strftime("%Y-%m-%d %H:%M:%S.%6N")
+    st = 12.months.ago.strftime("%Y-%m-%d %H:%M:%S.%6N")
     et = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
     if params.has_key?(:refresh) && params[:refresh]=="hard_refresh"
       @response = FetchData.get_datastreams(session,source="google-fit",datatype="com.personicle.individual.datastreams.interval.step.count",start_date=st, end_date=et, hard_refresh=true,uid=session[:oktastate]['uid'])
@@ -64,15 +64,64 @@ class MobilityController < ApplicationController
 
       temporary_steps = @response.group_by_day{|rec| rec['end_time'].to_datetime}.to_h 
       @mobility_aggregated = temporary_steps.map {|k,v| {k => v.sum {|r| r['value']}}}
+      #puts temporary_steps
+      puts "Maps temporary_steps (response grouped by day) hash to value summed for each day (steps)"
       # puts @mobility_aggregated
 
-      # Find how many total steps were taken in the last week
-      tmp_steps = @response.select {|record| record['end_time'].to_datetime > 30.days.ago}.map {|rec| [rec['end_time'].to_date, rec['value']]}.group_by {|r| r[0]}.to_h
+      # Find how many total steps were taken in the last month
+      tmp_steps = @response.select {|record| record['end_time'].to_datetime > 12.months.ago}.map {|rec| [rec['end_time'].to_date, rec['value']]}.group_by {|r| r[0]}.to_h
+      puts "Temporary Steps over Last 12 Months"
+      # puts tmp_steps
       @daily_steps = tmp_steps.map {|k,v| [k, v.sum {|r| r[1]}]}.to_h
-      # puts @daily_steps
+      puts "Daily Steps For Past 12 Months"
+      puts @daily_steps
       @last_week_total_steps = @daily_steps.select {|k,v| k > 7.days.ago}.sum {|k,v| v}
       puts "Number of Steps Taken All Last Week"
-      puts @last_week_total_steps 
+      # puts @last_week_total_steps 
+
+      puts "Add the date and value of 0 for days where there were no steps"
+        
+        range_for_daily_steps = @daily_steps.keys.first.beginning_of_month..@daily_steps.keys.last
+        puts "Range for Daily Steps (All 12 Months)"
+        puts range_for_daily_steps
+
+        @add_missing_days = {}
+        range_for_daily_steps.each do |date|
+          if @daily_steps.has_key?(date)
+            @add_missing_days[date] = @daily_steps[date]
+          else
+            @add_missing_days[date] = 0
+          end
+        end
+
+        puts "Get the Date Range for Each Month"
+        @grouped_by_month = @daily_steps.group_by_month { |date, steps| date.strftime('%Y-%m-%d') }
+        puts "Grouped By Month"
+        puts @grouped_by_month
+
+        @all_months_with_steps = {}
+        @grouped_by_month.each do |date, steps|
+          year = date.year
+          month = date.month
+
+          start_date = Date.new(year, month, 1)
+          end_date = (start_date >> 1) - 1
+          @date_range = start_date..end_date
+          puts "test"
+          puts @date_range
+          puts "#{date.strftime('%b %Y')}: #{@date_range}"
+          # puts "Each Month Daily"
+          @any_month_daily = @add_missing_days.select {|k,v| @date_range.include?(k)}
+          # puts @any_month_daily
+          @all_months_with_steps[date.strftime('%b %Y')] = @any_month_daily
+        end
+
+     
+  
+      puts "Each Month's Set Accessed Using date.strftime('%b %Y')"
+      puts @all_months_with_steps
+      puts "Hash with Zero Steps on Missing Dates"
+      puts @add_missing_days
     else
       @processed_steps_data = {}
       @mobility_aggregated = {}
@@ -103,20 +152,25 @@ class MobilityController < ApplicationController
     last_month_end_date = first_bar_start_date - 1.day
     puts "Last Month End Date"
     puts last_month_end_date
-    # Filter chart_data to only include data for the last week
+    # Filter chart_data to only include data for the last month
     chart_data = chart_data.select { |date, value| Date.parse(date) >= last_month_start_date && Date.parse(date) <= last_month_end_date }
     
     last_month_total_steps = @response.select { |record| Date.parse(record['end_time']) >= last_month_start_date && Date.parse(record['end_time']) <= last_month_end_date }.map { |rec| [Date.parse(rec['end_time']), rec['value']] }.group_by { |r| r[0] }.to_h
     puts "Last Month Steps"
     puts last_month_total_steps
-    last_month_daily_steps = last_month_total_steps.map { |k, v| [k, v.sum { |r| r[1] }] }.to_h
+    @last_month_daily_steps = last_month_total_steps.map { |k, v| [k, v.sum { |r| r[1] }] }.to_h
     puts "Last Month Daily Steps"
-    puts last_month_daily_steps
-    @daily_steps = last_month_daily_steps
+    puts @last_month_daily_steps
+    @daily_steps = @last_month_daily_steps
+
+    last_two_months_start_date = first_bar_start_date - 2.months
+    last_two_months_end_date = first_bar_start_date - 1.month
+    last_two_months_data = @response.select { |record| Date.parse(record['end_time']) >= last_two_months_start_date && Date.parse(record['end_time']) <= last_month_end_date }.map { |rec| [Date.parse(rec['end_time']), rec['value']] }.group_by { |r| r[0] }.to_h
+    @last_two_months_daily_steps = last_two_months_data.map { |k, v| [k, v.sum { |r| r[1] }] }.to_h
   
     respond_to do |format|
       format.html { redirect_back(fallback_location: root_path) }
-      format.json { render json: @daily_steps.to_json }
+      format.json { render json: { last_two_months_data: @last_two_months_daily_steps, last_month_data: @last_month_daily_steps, current_month_data: @daily_steps }.to_json }
     end
   end
 end
